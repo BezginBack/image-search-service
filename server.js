f8106d405621b85595389298ef86f15d0fb49ef3
System <none>
2017-07-25 13:22:37 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -2,15 +2,23 @@ var express = require("express");
 var app = express();
 var request = require("request");
 
-function parseIt(url, callback){
-  var arr = [];
+function parseIt(url, l, callback){
   request(url, function (err, page, body) {
     if (!err && page.statusCode == 200) {
       var obj = JSON.parse(body);
       var data = {
         item : []
       };
-      for (var i = 0; i < obj.items.length; i++){
+      var limit = 0;
+      if(l > 0){
+        limit = l;
+        if(l > 10){
+          limit = 10;
+        }
+      } else {
+        limit = obj.items.length;
+      }
+      for (var i = 0; i < limit; i++){
         data.item.push({
           url : obj.items[i].pagemap.imageobject[0].url,
           snippet : obj.items[i].snippet,
@@ -28,10 +36,14 @@ app.use(express.static('public'));
 
 app.get("/api", function (req, res) {
   if(req.query.imageSearch){
+    var limit = 0;
+    if(req.query.offset){
+      limit = req.query.offset 
+    }
     var q = req.query.imageSearch;
     var url = 'https://www.googleapis.com/customsearch/v1?cx='+ process.env.ID + '&key=' + process.env.KEY + '&q=' + q;
     res.writeHead(200, {"content-type" : "text/plain"});
-    parseIt(url, function(err, data){
+    parseIt(url, limit, function(err, data){
       if(err) res.end("err " + err);
       res.end(JSON.stringify(data));
     });
